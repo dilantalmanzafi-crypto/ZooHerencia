5fe194a147e6a83fe036e64f69ceab2be9be55cf
DILANT STEVEN ALMANZA FIGUEROA <dilant_almanzafi@fet.edu.co>
2025-09-12 20:04:38 -0500

    Create Collar.java

---
+++ ZooHerencia/Collar.java
@@ -0,0 +1,14 @@
+package zooherencia;
+
+public class Collar {
+    private String color;
+
+    public Collar(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Collar[color=" + color + "]";
+    }
+}
